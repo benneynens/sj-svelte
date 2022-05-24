54c9de6e44ab99eec9a3d57bb77a132650efd54f
Ben Neynens <benneynens@gmail.com>
2022-05-24 16:50:29 +0800

    change data source for testimonialss

--- src/routes/testimonials.svelte
+++ src/routes/testimonials.svelte
@@ -10,30 +10,30 @@
     }
   }
 }`;
-      export async function load({ fetch }) {
-      const response = await fetch(import.meta.env.VITE_PUBLIC_WORDPRESS_API_URL, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ query }),
-      });
-          if (response.ok) {
-        const responseObj = await response.json();
-        // console.log (responseObj);
-        const posts = responseObj.data.pages.edges;
-        // console.log (posts)
-              return {
-                  props: {
-                      posts
-                  }
-              };
-          }
-          return {
-              status: response.status,
-              error: new Error(`Could not load ${url}`)
-          };
-      }
+      // export async function load({ fetch }) {
+      // const response = await fetch(import.meta.env.VITE_PUBLIC_WORDPRESS_API_URL, {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify({ query }),
+      // });
+      //     if (response.ok) {
+      //   const responseObj = await response.json();
+      //   // console.log (responseObj);
+      //   const posts = responseObj.data.pages.edges;
+      //   // console.log (posts)
+      //         return {
+      //             props: {
+      //                 posts
+      //             }
+      //         };
+      //     }
+      //     return {
+      //         status: response.status,
+      //         error: new Error(`Could not load ${url}`)
+      //     };
+      // }
   </script>
   
   
@@ -43,7 +43,7 @@
 
       async function handleSubmit() {
         try {
-          const response = await fetch(import.meta.env.VITE_PUBLIC_WORDPRESS_API_URL, {
+          const response = await fetch(import.meta.env.VITE_PUBLIC_IHUB_URL, {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
